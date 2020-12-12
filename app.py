4d04f8e326fff75d29c39fb8ddecf6895be2d5bc
Calum Hamilton <hamcalum10@gmail.com>
2020-12-12 12:31:54 +0000

    Reached chapter 6 - routing, file handling, error handling etc. included

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # All code examples are at https://github.com/siwells/set09103
 
 # Running on 127.0.0.1:5000 currently
-from flask import Flask
+from flask import Flask, redirect, url_for, abort
 
 app = Flask(__name__)
 
@@ -10,6 +10,7 @@ app = Flask(__name__)
 @app.route('/home/')
 def hello_world():
     return('Hello, World!')
+    return('Welcome to my homepage!')
 
 @app.route('/profile/')
 def profile():
@@ -19,6 +20,29 @@ def profile():
 @app.route('/about/')
 def aboutme():
     return('All about me!')
+
+@app.route('/private/')
+def private():
+    return redirect(url_for('login'))
+
+@app.route('/login/')
+def login():
+    return('Please enter a username and password:')
+
+@app.route('/image/')
+@app.route('/picture/')
+def image():
+    return ('<img src=https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Breathe-face-smile.svg/1200px-Breathe-face-smile.svg.png>', 200)
+
+
+
+@app.route('/force404/')
+def force404():
+    abort(404)
+
+@app.errorhandler(404)
+def error_page_not_found(error):
+    return("Sorry, that page doesn't exist I'm afraid!", 404)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
