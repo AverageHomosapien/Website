3bf90ac00bc8a6ca49f8d29e7e716fe6f99265df
Calum <calum.hamilton@outlook.com>
2020-12-13 16:38:30 +0000

    Template site up and running - need to convert to base templates/source own images etc.

--- app/app.py
+++ app/app.py
@@ -4,11 +4,22 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 Bootstrap(app)
 
-@app.route('/')
 @app.route('/home/')
+@app.route('/index/')
+@app.route('/')
 def home():
-    #return render_template('second_home.jinja')
+    print("Url for {}".format(url_for('static', filename='owl-carousel/owl.carousel.css')))
     return render_template('index.jinja')
+
+@app.route('/contact/')
+@app.route('/contact-us/')
+def contact():
+    return render_template('contact.jinja')
+
+@app.route('/about/')
+@app.route('/about-me/')
+def about():
+    return render_template('about.jinja')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
